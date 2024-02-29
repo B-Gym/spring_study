a015cc99796b38c91cd37c96683f6d86a6fa4479
kimgoat <kimgoatlove@gmail.com>
2024-02-29 16:27:23 +0900

    JUnit framwork를 사용한 테스트

---
+++ section1/src/test/java/study/section1/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package study.section1.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "kimgoat", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
