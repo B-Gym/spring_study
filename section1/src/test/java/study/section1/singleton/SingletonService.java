668e5cd577485f78d5319464e1e0a039978cf3b1
kimgoat <kimgoatlove@gmail.com>
2024-03-04 14:39:10 +0900

    싱글톤 패턴을 구현하여 객체를 미리 생성하여 해당 객체를 공유

---
+++ section1/src/test/java/study/section1/singleton/SingletonService.java
@@ -0,0 +1,24 @@
+package study.section1.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonService {
+    // 1. static 영역에 객체를 딱 1개만 생성해둔다.
+    private static final SingletonService instance = new SingletonService();
+
+    // 2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용한다.
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+
+    // 3. 생성자를 private로 선언하여 외부에서 new 키워드를 사용한 객체 생성을 못하게 막는다.
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("싱클톤 객체 로직 호출");
+    }
+
+}
