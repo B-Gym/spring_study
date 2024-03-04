1b1fc5b197ed65492cf5e515a1eb30e6a7b971a1
kimgoat <kimgoatlove@gmail.com>
2024-03-04 13:52:50 +0900

    다양한 형식 설정 지원 - xml

---
+++ section1/src/test/java/study/section1/xml/XmlAppcontext.java
@@ -0,0 +1,18 @@
+package study.section1.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import study.section1.member.MemberService;
+
+public class XmlAppcontext {
+
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
