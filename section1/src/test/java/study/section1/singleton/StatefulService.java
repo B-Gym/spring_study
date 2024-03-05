a477bcc4c19292fba0fb8f79b853197e98ec1420
kimgoat <kimgoatlove@gmail.com>
2024-03-05 17:55:25 +0900

    싱글톤 방식의 문제를 해결하는 아주 간단하고 단순한 방법

--- section1/src/test/java/study/section1/singleton/StatefulService.java
+++ section1/src/test/java/study/section1/singleton/StatefulService.java
@@ -1,15 +1,14 @@
 package study.section1.singleton;
 
 public class StatefulService {
-    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price){
+    public int order(String name, int price){
         System.out.println("name = " + name +" price = " + price);
-        this.price = price; // 여기가 문제
-    }
-
-    public int getPrice(){
         return price;
     }
+
+//    public int getPrice(){
+//        return price;
+//    }
 
 }
--- section1/src/test/java/study/section1/singleton/StatefulServiceTest.java
+++ section1/src/test/java/study/section1/singleton/StatefulServiceTest.java
@@ -15,16 +15,13 @@ public class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA: A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         // ThreadB: B사용자 20000원 주문
-        statefulService1.order("userB", 20000);
+        int userBPrice = statefulService1.order("userB", 20000);
 
         // ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
-
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
     }
 
     static class TestConfig {
